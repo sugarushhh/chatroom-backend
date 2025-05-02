76c7564fd537f0d6b1a4841b8763e1990e07dd99
sugarushhh <zcr102307@163.com>
2025-05-02 10:56:29 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express');
+const path = require('path');
+const app = express();
+
+// 提供静态文件
+app.use(express.static(path.join(__dirname, 'public')));
+
+const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const cors = require('cors');
